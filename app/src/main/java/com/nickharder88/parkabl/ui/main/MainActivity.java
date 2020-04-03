1fc4d93aabb702b9e0c2016ffc3edd6d21703e2d
Nick Harder <nick@abstraconsulting.com>
2020-04-03 10:32:28 -0400

    add firebase performance and crashlytics, fix bugs

--- app/src/main/java/com/nickharder88/parkabl/ui/main/MainActivity.java
+++ app/src/main/java/com/nickharder88/parkabl/ui/main/MainActivity.java
@@ -50,7 +50,8 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.homeFragment)
+                R.id.homeFragment,
+                R.id.homeNextFragment)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
