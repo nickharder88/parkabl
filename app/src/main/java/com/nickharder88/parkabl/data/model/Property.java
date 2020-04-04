c7e1e2921e10973a6558fc1bf66a3c274f31b28d
Nick Harder <nick@abstraconsulting.com>
2020-04-03 21:16:06 -0400

    add address card

--- app/src/main/java/com/nickharder88/parkabl/data/model/Property.java
+++ app/src/main/java/com/nickharder88/parkabl/data/model/Property.java
@@ -6,6 +6,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.nickharder88.parkabl.data.dto.AddressDTO;
+import com.nickharder88.parkabl.data.dto.LandlordDTO;
 import com.nickharder88.parkabl.data.dto.PropertyDTO;
 
 public class Property {
@@ -25,7 +26,6 @@ public class Property {
         }
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         return db.collection(Address.collection).document(data.address).get().continueWith(new Continuation<DocumentSnapshot, Address>() {
             @Override
             public Address then(@NonNull Task<DocumentSnapshot> task) throws Exception {
@@ -43,6 +43,33 @@ public class Property {
                 }
 
                 return new Address(snap.getId(), snap.toObject(AddressDTO.class));
+            }
+        });
+    }
+
+    public Task<Landlord> getLandlord() {
+        if (data.landlord == null) {
+            return null;
+        }
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        return db.collection(Landlord.collection).document(data.landlord).get().continueWith(new Continuation<DocumentSnapshot, Landlord>() {
+            @Override
+            public Landlord then(@NonNull Task<DocumentSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    if (task.getException() != null) {
+                        throw task.getException();
+                    }
+
+                    throw new Exception("Could not get Landlord");
+                }
+
+                DocumentSnapshot snap = task.getResult();
+                if (snap == null) {
+                    return null;
+                }
+
+                return new Landlord(snap.getId(), snap.toObject(LandlordDTO.class));
             }
         });
     }
--- app/src/main/java/com/nickharder88/parkabl/ui/main/property/PropertyFragment.java
+++ app/src/main/java/com/nickharder88/parkabl/ui/main/property/PropertyFragment.java
@@ -1,36 +1,55 @@
 package com.nickharder88.parkabl.ui.main.property;
 
 import android.os.Bundle;
-import android.widget.TextView;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Observer;
 import com.nickharder88.parkabl.R;
+import com.nickharder88.parkabl.data.model.Property;
+import com.nickharder88.parkabl.ui.main.fragments.cards.CardAddressFragment;
 
 public class PropertyFragment extends Fragment {
 
-    public PropertyFragment() {
-        // Required empty public constructor
-    }
+    private PropertyViewModel viewModel;
+
+    public PropertyFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_property, container, false);
 
-        TextView property = view.findViewById(R.id.text_view_property);
-        property.setText(getArguments().getString("propertyId"));
+        final String propertyId = getArguments().getString("propertyId");
+        if (propertyId == null) {
+            throw new Error("Expected Property Id to be passed to Property Fragment");
+        }
 
         // Add Child Fragments
         FragmentManager fm = getChildFragmentManager();
         Fragment scan = fm.findFragmentById(R.id.fragment_scan);
+        CardAddressFragment cardAddress = (CardAddressFragment) fm.findFragmentById(R.id.fragment_card_address);
 
         if (scan == null) {
             fm.beginTransaction().add(R.id.fragment_scan, new ScanFragment()).commit();
         }
+
+        if (cardAddress == null) {
+            cardAddress = new CardAddressFragment();
+            fm.beginTransaction().add(R.id.fragment_card_address, cardAddress).commit();
+        }
+
+        final CardAddressFragment finalCardAddress = cardAddress;
+        viewModel = new PropertyViewModel(propertyId);
+        viewModel.getProperty().observe(getViewLifecycleOwner(), new Observer<Property>() {
+            @Override
+            public void onChanged(Property property) {
+                finalCardAddress.setAddress(property.data.address);
+            }
+        });
 
         return view;
     }
